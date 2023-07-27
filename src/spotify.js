a819b1f6cf204c7eed5098c303fb818f4c18a01f
hanuniye <hanuniye@gmail.com>
2023-07-27 10:45:08 +0300

    v1 commit

--- src/spotify.js
+++ src/spotify.js
@@ -1,6 +1,6 @@
 const authEndpoint = "https://accounts.spotify.com/authorize";
 const clientId = "f1b868de105e49f7b30769853ea99786";
-const redirectUri = process.env.ENVIRO ? "https://spotify-zeta-lyart.vercel.app/" : "http://localhost:5173/" ;
+const redirectUri = process.env.ENVIRO === "production" ? "https://spotify-zeta-lyart.vercel.app/" : "http://localhost:5173/" ;
 
 const permissions = [
   "user-read-currently-playing",
